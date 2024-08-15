7e78d57208b2695ce01319f4a47e2fd1a97635c0
Kriya <kriya101101@gmail.com>
2024-08-15 17:34:34 +0800

    Add basic structure for user login and register

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  return res.redirect('/records')
+  return res.redirect('/login')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/login', (req, res) => {
+  res.send(req.body)
+})
+
+router.post('/logout', (req, res) => {
+  res.redirect('/login')
 })
 
 
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,36 @@ const router = express.Router();
 const db = require('../models')
 const User = db.User
 
+router.post('/', (req, res, next) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  if (!email || !password) {
+    req.flash('error', '請輸入email及密碼')
+    return res.redirect('back')
+  }
+  if (password !== confirmPassword) {
+    req.flash('error', '密碼及驗證碼不一致')
+    return res.redirect('back')
+  }
+  
+  return User.findOne({where: { email }})
+    .then((user) => {
+      if (user) { 
+        req.flash('error', '此email已註冊')
+        return res.redirect('back')
+      }
+      return User.create({ name, email, password })
+    })
+    .then((user) => {
+      if (!user) { return res.redirect('back') }
+
+      req.flash('success', '註冊成功')
+      return res.redirect('/login')
+    })
+    .catch((error) => {
+      error.errorMessage = '註冊失敗';
+      next(error)
+    });
+})
+
 module.exports = router
