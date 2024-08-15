c3da71c6daec064a52b946c70ccd642185a64b1a
Kriya <kriya101101@gmail.com>
2024-08-15 12:01:02 +0800

    Modify edit.hbs select input

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const router = require('./routes');
 
 const { engine } = require('express-handlebars');
 const methodOverride = require('method-override');
+const handlebars = require('handlebars');
+
+const session = require('express-session');
+const flash = require('connect-flash');
+const messageHandler = require('./middlewares/message-handler')
 
 const port = 3000;
+
+handlebars.registerHelper('eq', (arg1, arg2) => {
+  return arg1 === arg2
+})
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -15,8 +24,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-app.use(router);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
 
+app.use(flash());
+app.use(messageHandler);
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`)
